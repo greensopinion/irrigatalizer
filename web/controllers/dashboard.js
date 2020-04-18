9244a0d14411a67143a33486585d238f6f65a3b7
David Green <greensopinion@gmail.com>
2020-04-18 10:52:54 -0700

    add initial dashboard mockup

--- web/controllers/dashboard.js
+++ web/controllers/dashboard.js
@@ -1,7 +1,21 @@
 exports.handler = async (req, res) => {
   try {
+    let now = Date.now();
     let model = {
       title: "dashboard",
+      status: {
+        running: true,
+        current: {
+          circuit: 3,
+          start: new Date(now - 60 * 1000 * 5).toISOString(),
+          end: new Date(now + 60 * 1000 * 25).toISOString(),
+        },
+        next: {
+          circuit: 3,
+          start: new Date(now + 60 * 1000 * 25).toISOString(),
+          end: new Date(now + 60 * 1000 * 55).toISOString(),
+        },
+      },
     };
     res.render("dashboard", model);
   } catch (e) {
