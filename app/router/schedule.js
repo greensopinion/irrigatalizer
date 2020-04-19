aecebc8fe0872617a983f58fabb2d8b9255acc18
David Green <greensopinion@gmail.com>
2020-04-19 16:22:00 -0700

    implement schedule persistence

--- app/router/schedule.js
+++ app/router/schedule.js
@@ -1,14 +1,26 @@
+const router = require("express").Router();
 
-const router = require('express').Router();
+const configurationService = require("../domain/configuration-service");
 
 const statusHandler = async (request, response) => {
-  let date = new Date();
-  response.status(200).json({ placeholder: "true" });
+  const configuration = await configurationService.retrieve();
+  response.status(200).json(configuration);
 };
 
-router.get('/', statusHandler);
+const updateHandler = async (request, response) => {
+  const model = request.body;
+  if (!model.schedule) {
+    response.status(400).json({ error: "Expected schedule" });
+  } else {
+    await configurationService.update(model);
+    response.status(200).json({});
+  }
+};
+
+router.get("/", statusHandler);
+router.put("/", updateHandler);
 
 module.exports = {
   router,
-  statusHandler
+  statusHandler,
 };
--- app/router/schedule.spec.js
+++ app/router/schedule.spec.js
@@ -1,35 +1,88 @@
 const { router } = require("./schedule");
 const request = require("supertest");
+var bodyParser = require("body-parser");
 const express = require("express");
 
+jest.mock("../domain/configuration-service");
+const configurationService = require("../domain/configuration-service");
+
 describe("schedule", () => {
-    it("should provide a router", () => {
-        expect(router).toBeDefined();
+  it("should provide a router", () => {
+    expect(router).toBeDefined();
+  });
+
+  describe("conroller", () => {
+    let app;
+
+    beforeEach(() => {
+      app = express();
+      app.use(bodyParser.json());
+      app.use("/", router);
     });
 
-    describe("conroller", () => {
-        let app;
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
 
-        beforeEach(() => {
-            app = express();
-            app.use("/", router);
-        });
-        it("should provide a schedule", done => {
-            request(app)
-                .get("/")
-                .expect("Content-Type", "application/json; charset=utf-8")
-                .expect(200)
-                .end((err, response) => {
-                    if (err) {
-                        return done(err);
-                    }
-                    expect(response.body).toMatchObject(
-                        { 
-                            placeholder: "true"
-                        }
-                    );
-                    done();
-                });
+    it("should provide a schedule", (done) => {
+      const mockConfiguration = {
+        schedule: ["mock-schedule"],
+      };
+      configurationService.retrieve = async () => {
+        return mockConfiguration;
+      };
+
+      request(app)
+        .get("/")
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .expect(200)
+        .end((err, response) => {
+          if (err) {
+            return done(err);
+          }
+          expect(response.body).toEqual(mockConfiguration);
+          done();
         });
     });
+
+    it("should put a schedule", (done) => {
+      const mockConfiguration = {
+        schedule: ["mock-schedule"],
+      };
+
+      request(app)
+        .put("/")
+        .send(mockConfiguration)
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .expect(200)
+        .end((err, response) => {
+          if (err) {
+            return done(err);
+          }
+          expect(response.body).toEqual({});
+          expect(configurationService.update).toHaveBeenCalledWith(
+            mockConfiguration
+          );
+          done();
+        });
+    });
+
+    it("rejects a schedule update that does not have a schedule", (done) => {
+      const mockConfiguration = {};
+
+      request(app)
+        .put("/")
+        .send(mockConfiguration)
+        .expect("Content-Type", "application/json; charset=utf-8")
+        .expect(400)
+        .end((err, response) => {
+          if (err) {
+            return done(err);
+          }
+          expect(response.body).toEqual({ error: "Expected schedule" });
+          expect(configurationService.update).not.toHaveBeenCalled();
+          done();
+        });
+    });
+  });
 });
