8adeffff10ba5536f52e8306a5d9fe6ec1efb1eb
David Green <greensopinion@gmail.com>
2020-05-02 17:14:16 -0700

    add history timeline to dashboard

--- web/controllers/dashboard.js
+++ web/controllers/dashboard.js
@@ -1,4 +1,5 @@
 const scheduleService = require("../../app/schedule-service");
+const historyService = require("../../app/domain/history-service");
 
 const entryToViewModel = (item) => {
   return {
@@ -11,6 +12,7 @@ const entryToViewModel = (item) => {
 exports.handler = async (req, res) => {
   try {
     const schedule = await scheduleService.retrieve();
+    const history = await historyService.readEntries();
 
     const current = schedule.current
       ? entryToViewModel(schedule.current)
@@ -24,6 +26,11 @@ exports.handler = async (req, res) => {
         current,
         next,
       },
+      history: history.map((e) => {
+        e.start = Date.parse(e.startTime);
+        e.end = Date.parse(e.endTime);
+        return e;
+      }),
     };
     res.render("dashboard", model);
   } catch (e) {
